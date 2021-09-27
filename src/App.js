869e9548f54a62a84a7762097952efc80c7bacf1
Sinyor <abdurahmanov.togrul@gmail.com>
2021-09-27 22:40:46 +0300

    create project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { SafeAreaView, View } from 'react-native'
+function App() {
+
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
